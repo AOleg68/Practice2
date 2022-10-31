37155762b90714cc104b024c930c6ba202e6d1fa
Oleg <o.antonenko2011@yandex.ru>
2022-10-31 13:20:50 +0300

    Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+int Number = new Random().Next (1, 5000000);
+Console.Write ("Число:  ");
+Console.WriteLine (Number);
+
+if (Number >= 100)
+{
+    while (Number >= 100 ) Number /= 10;
+    {
+        int Tretye = (Number % 10);
+        Console.Write ("Третья цифра:");
+        Console.WriteLine (Tretye);
+    }
+}
+else
+{
+    Console.Write ("Третьей цифры нет!");
+}
+
+
+
