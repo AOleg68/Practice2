1dad194584d6ad0d3699324e1c1be5bca27d3261
Oleg <o.antonenko2011@yandex.ru>
2022-10-31 11:46:31 +0300

    Задача 1

---
+++ Задача 1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number = new Random().Next (100, 999);
+Console.Write ("Число:  ");
+Console.WriteLine (Number);
+
+int Center = (Number % 100) / 10;
+
+Console.Write ("Вторая цифра числа:");
+Console.WriteLine (Center);
+
+
+
+
