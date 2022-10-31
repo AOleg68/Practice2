bf5919a61fa08b9b837b1b4f40b0162df504bba9
Oleg <o.antonenko2011@yandex.ru>
2022-10-31 13:35:50 +0300

    Fix Задачи 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,13 +1,12 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-int Number = new Random().Next (1, 5000000);
-Console.Write ("Число:  ");
-Console.WriteLine (Number);
+Console.Write ("Введите число:  ");
+int Number = Convert.ToInt32 (Console.ReadLine());
 
 if (Number >= 100)
 {
-    while (Number >= 100 ) Number /= 10;
+    while (Number >= 999 ) Number /= 10;
     {
         int Tretye = (Number % 10);
         Console.Write ("Третья цифра:");
